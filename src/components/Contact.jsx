6f49c59ebfccf1812c6dcb55c8c6e9e1f8fc537d
Alex <8157518@gmail.com>
2025-11-23 22:14:49 +0200

    Contact form initial design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,33 @@
 const Contact = () => {
     return (
         <div className={'far-galaxy'}>
-            Contact
+            <div className="container-form">
+                <form action="">
+
+                    <label htmlFor="fname">First Name</label>
+                    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+                    <br/>
+
+                    <label htmlFor="lname">Last Name</label>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                    <br/>
+
+                    <label htmlFor="country">Country</label>
+                    <select id="country" name="country">
+                        <option value="" disabled selected hidden>Select country</option>
+                        <option value="australia">Australia</option>
+                        <option value="canada">Canada</option>
+                        <option value="usa">USA</option>
+                    </select>
+
+                    <div className="subjectContainer">
+                        <label htmlFor="subject">Subject</label>
+                        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+                    </div>
+                    <input type="submit" value="Submit" className="btn btn-danger mx-1 border-warning"/>
+
+                </form>
+            </div>
         </div>
     )
 }
